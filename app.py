540f3bb1a70a189d136fbd5bc169c71b5ae14bb1
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2024-06-18 20:32:46 +0530

    added structure with logging code

---
+++ app.py
@@ -0,0 +1,3 @@
+from Text_To_Speech_SRC.logger import logging
+
+logging.info("Hi this is test")
---
+++ template.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+project_name="Text_To_Speech_SRC"
+
+list_of_files=[
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"requirements.py",
+    "Dockerfile",
+    ".dockerignore",
+    "app.py"
+]
+
+
+for file in list_of_files:
+    filepath=Path(file)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+    
+    if not (os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+    else:
+        print("file already exists")
+    
